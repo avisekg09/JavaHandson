d006edef6b391ee9d3d7119696c5b77fc723448b
avisekg09 <avisekg09@gmail.com>
2024-08-03 17:15:46 +0530

    SavingsAccountDepositWithdraw

---
+++ src/main/java/com/ArrayListPractice/SavingsAccountDepositWithdraw.java
@@ -0,0 +1,38 @@
+package com.ArrayListPractice;
+
+public class SavingsAccountDepositWithdraw {
+
+	static int amount;
+	public static void main(String[] args) {
+		amount = 100;
+		System.out.println(checkBalance());
+		deposit(200);
+		withdraw(100);
+	}
+	
+	
+	
+	public static boolean deposit(int depAmount) {
+		if(depAmount<0) {
+			System.out.println("Deposit not allowed for negative amount");
+			return false;
+		}else {
+			amount = amount+depAmount;
+			System.out.println("Deposit successfull-"+amount);
+			return true;
+		}
+	}
+	public static boolean withdraw(int withAmount) {
+		if(withAmount>amount || withAmount<0) {
+			System.out.println("Withdrawl not possible");
+			return false;
+		}else {
+			amount = amount - withAmount;
+			System.out.println("Withdrawl Successfull-"+amount);
+			return true;
+		}
+	}
+	public static int checkBalance() {
+		return amount;
+	}
+}
