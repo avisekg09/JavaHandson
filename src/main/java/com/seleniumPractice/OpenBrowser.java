f9cb7aaee9b52115a7b8f775ad73e7c34cae0a5e
avisekg09 <avisekg09@gmail.com>
2024-08-21 21:56:57 +0530

    updated OpenBrowser code

--- src/main/java/com/seleniumPractice/OpenBrowser.java
+++ src/main/java/com/seleniumPractice/OpenBrowser.java
@@ -1,6 +1,6 @@
 package com.seleniumPractice;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
 
 import com.sun.tools.javac.launcher.Main;
 
@@ -9,23 +9,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.Scanner;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+/*
+ * Write a script for the following:
+ Open the browser
+ Delete all cookies
+ Set size of the window
+ Set position of the window
+ Maximize the window
 
+ */
 public class OpenBrowser {
 
-	public WebDriver driver;
+	public static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Browser");
-		String broswerName  = sc.next();
+		String broswerName =  "Chrome";//= sc.next();
 		OpenBrowser ob = new OpenBrowser();
-		ob.launchBrowser(broswerName);
-		
-		
+		ob.Driver(broswerName).get("https://www.google.com");
+		driver.manage().deleteAllCookies();
+		org.openqa.selenium.Dimension d = new Dimension(500,400);
+		driver.manage().window().setSize(d);
+		Point p = new Point(50,500);
+		driver.manage().window().setPosition(p);
+		driver.navigate().refresh();
 		
 	}
-	public WebDriver launchBrowser(String browserName) {
+	public WebDriver Driver(String browserName) {
 		if(browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
